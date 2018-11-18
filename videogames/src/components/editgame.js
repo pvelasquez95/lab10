ad9ffd0e4f55e61d26c1565f7c0b77604f647382
pvelasquez95 <pablovelasquez95@hotmail.com>
2018-11-18 13:47:22 -0600

    dockerization

---
+++ videogames/src/components/editgame.js
@@ -0,0 +1,98 @@
+//import the necessary files
+import React from 'react';
+import { Modal, ControlLabel, FormGroup, FormControl, Button } from 'react-bootstrap';
+//create a class for displaying the modal for editing an existing game and export it
+export class EditGame extends React.Component {
+    constructor(props) {//create a state to handle the game to be edited
+        super(props);
+        this.state = { name: "", platform: "", rate: "", photo: "" };
+        this.handleGameNameChange = this.handleGameNameChange.bind(this);
+        this.handleGamePlatformChange = this.handleGamePlatformChange.bind(this);
+        this.handleGameRateChange = this.handleGameRateChange.bind(this);
+        this.handleGamePhotoChange = this.handleGamePhotoChange.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+    }
+    static getDerivedStateFromProps(props, state) {//make the game prop a state
+        const prevName = state.prevName;
+        const prevPlatform = state.prevPlatform;
+        const prevRate = state.prevRate;
+        const prevPhoto = state.prevPhoto;
+        const name = (prevName !== props.game.name) ? props.game.name : state.name;
+        const platform = prevPlatform !== props.game.platform ? props.game.platform : state.platform;
+        const rate = prevRate !== props.game.rate ? props.game.rate : state.rate;
+        const photo = prevPhoto !== props.game.photo ? props.game.photo : state.photo;
+        return {
+            prevName: props.game.name, name,
+            prevPlatform: props.game.platform, platform,
+            prevRate: props.game.rate, rate,
+            prevPhoto: props.game.photo, photo,
+        }
+    }
+    handleGameNameChange(e) {//change the name to reflect user input
+        this.setState({ name: e.target.value });
+    }
+    handleGamePlatformChange(e) {//change the platform to reflect user input
+        this.setState({ platform: e.target.value });
+    }
+    handleGameRateChange(e) {//change the rate to reflect user input
+        this.setState({ rate: e.target.value });
+    }
+    handleGamePhotoChange(e) {//change the photo to reflect user input
+        this.setState({ photo: e.target.value });
+    }
+    handleEdit(e) {//get the game data, manipulate it and call the function for editing an existing game
+        e.preventDefault();
+        const onEdit = this.props.onEdit;
+        const currentlyEditing = this.props.currentlyEditing;
+        var name = this.state.name;
+        var platform = this.state.platform;
+        var rate = this.state.rate;
+        var photo = this.state.photo;
+        onEdit(name, platform, rate, photo, currentlyEditing);
+    }
+    handleCancel() {
+        const onEditModal = this.props.onEditModal;
+        this.setState({
+            name: this.props.game.name, platform: this.props.game.platform,
+            rate: this.props.game.rate, photo: this.props.game.photo
+        });
+        onEditModal();
+    }
+    render() {
+        const onShow = this.props.onShow;
+        var regex1 = /^\S/;
+        const validGame = regex1.test(this.state.name) && regex1.test(this.state.platform)
+            && regex1.test(this.state.rate) && regex1.test(this.state.photo);
+        return (
+            <div>
+                <Modal show={onShow} onHide={this.handleCancel} animation={false}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Edit Game</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <FormGroup controlId="formControlsName">
+                            <ControlLabel>Game Name</ControlLabel>
+                            <FormControl type="text" required onChange={this.handleGameNameChange} value={this.state.name} placeholder="Enter Name" />
+                        </FormGroup>
+                        <FormGroup controlId="formControlsPlatform">
+                            <ControlLabel>Game Platform</ControlLabel>
+                            <FormControl type="text" required onChange={this.handleGamePlatformChange} value={this.state.platform} placeholder="Enter Platform" />
+                        </FormGroup>
+                        <FormGroup controlId="formControlsRate">
+                            <ControlLabel>Game Rate</ControlLabel>
+                            <FormControl type="text" required onChange={this.handleGameRateChange} value={this.state.rate} placeholder="Enter Rate" />
+                        </FormGroup>
+                        <FormGroup controlId="formControlsPhoto">
+                            <ControlLabel>Game Photo</ControlLabel>
+                            <FormControl type="text" required onChange={this.handleGamePhotoChange} value={this.state.photo} placeholder="Enter Photo" />
+                        </FormGroup>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button disabled={!validGame} bsStyle="success" onClick={this.handleEdit}>Save Recipe</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
+    }
+};
---
+++ videogames/src/index.js
@@ -0,0 +1,155 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ImageHeader } from "react-simple-card";
+import { AddGame } from './components/addgame';
+import { EditGame } from './components/editgame';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+class Games extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            games: [],
+            showAdd: false,
+            showEdit: false,
+            currentlyEditing: 0
+        };
+        this.showAddModal = this.showAddModal.bind(this);
+        this.showEditModal = this.showEditModal.bind(this);
+        this.addGame = this.addGame.bind(this);
+        this.editGame = this.editGame.bind(this);
+        this.deleteGame = this.deleteGame.bind(this);
+        
+    }
+    componentDidMount() {//load the local storage data after the component renders
+        const url = 'http://localhost:3001/api/juegos/';
+        fetch(url,{method: 'GET'})
+        .then(response => response.json())
+        .then(json => {
+            const list = Object.values(json.games)
+            this.setState({ games: list});
+        })
+        
+        /*
+        var games = (typeof localStorage["games"] !== "undefined") ? JSON.parse(localStorage.getItem("games")) : [
+            { name: "Dota 2", platform: ["PC"], rate: ["7"], photo: ['https://files.sirclocdn.xyz/kudouanimanga/products/_170803170241_21%20dota2_tn.jpg'] },
+            { name: "Mario Odyssey", platform: ["NS"], rate: ["9.5"], photo: ['https://upload.wikimedia.org/wikipedia/en/thumb/8/8d/Super_Mario_Odyssey.jpg/220px-Super_Mario_Odyssey.jpg'] },
+            { name: "Zelda Breath of the Wild", platform: ["NS"], rate: ["9"], photo: ['https://upload.wikimedia.org/wikipedia/en/thumb/c/c6/The_Legend_of_Zelda_Breath_of_the_Wild.jpg/220px-The_Legend_of_Zelda_Breath_of_the_Wild.jpg'] },
+            { name: "Sea of Thieves", platform: ["XBOX"], rate: ["5"], photo: ['https://store-images.s-microsoft.com/image/apps.47844.14554784103656548.c603f1f6-32b9-41dc-8f67-41374fe8b6db.e1b20439-8be8-46e6-890a-f05a6cae6d1c?mode=crop&q=90&h=300&w=200'] },
+            { name: "State of Decay 2", platform: ["XBOX"], rate: ["4"], photo: ['https://i11d.3djuegos.com/juegos/13406/state_of_decay_2/fotos/ficha/state_of_decay_2-3753095.jpg'] },
+            { name: "Overwatch", platform: ["PC"], rate: ["7.5"], photo: ['https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Overwatch_cover_art.jpg/220px-Overwatch_cover_art.jpg'] },
+            { name: "Persona 5", platform: ["PS4"], rate: ["8.5"], photo: ['https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Persona_5_cover_art.jpg/220px-Persona_5_cover_art.jpg'] },
+            { name: "Digimon Cyber Sleuth", platform: ["PS4"], rate: ["8"], photo: ['https://k61.kn3.net/taringa/4/7/2/3/7/3//gonza491/330x330_0BB.jpg'] }
+        ];
+        this.setState({ games: games });*/
+
+    }
+    showAddModal() {
+        this.setState({ showAdd: !this.state.showAdd });
+    }
+    showEditModal(index) {
+        {console.log(index)}
+        this.setState({ showEdit: !this.state.showEdit, currentlyEditing: index});
+    }
+    addGame(game) {
+        let games = this.state.games;
+        games.push(game);
+        const url = 'http://localhost:3001/api/juegos/';
+        console.log(game)
+        console.log(JSON.stringify(game))
+        fetch(url,{method: 'POST',
+        body:JSON.stringify(game),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    })
+        this.setState({ games: games });
+        this.showAddModal();
+    }
+    editGame(newName, newPlatform, newRate, newPhoto, currentlyEditing) {//edit an existing recipe
+        let games = this.state.games;
+        console.log("editGame "+currentlyEditing);
+        console.log("editGame "+games[currentlyEditing]._id);
+        games[currentlyEditing] = {_id: games[currentlyEditing]._id, name: newName, platform: newPlatform, rate: newRate, photo: newPhoto };
+        var game = {_id: games[currentlyEditing]._id, name: newName, platform: newPlatform, rate: newRate, photo: newPhoto };
+        console.log("game "+game);
+        const url = 'http://localhost:3001/api/juegos/';
+        fetch(url,{method: 'PUT',
+        body:JSON.stringify(game),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+        })
+        this.setState({ games: games });
+        this.showEditModal(currentlyEditing);
+    }
+    deleteGame(index) {//delete an existing game
+        let games = this.state.games;
+        
+        console.log(games[index]._id);
+        const url = 'http://localhost:3001/api/juegos/'+games[index]._id;
+        console.log("deleting: "+ index);
+        fetch(url,{method: 'DELETE'/*,
+    body:JSON.stringify({id:index})*/ })
+        
+        games.splice(index, 1);
+        this.setState({ games: games, currentlyEditing: 0 });
+    }
+    render() {
+        const {games} = this.state
+        return (
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="neon">All the videogames I've loved</div>
+                    <div className="flux">♥ ♥ ♥ ♥ ♥</div>
+                </div>
+
+                <div className="text-center">
+                    <button type="button" className="btn btn-labeled btn-primary btn-letra" onClick={this.showAddModal}>
+                        <span className="btn-label"><i className="glyphicon glyphicon-plus"></i></span>   Add Game   </button>
+                    <br />
+                </div>
+                <AddGame onShow={this.state.showAdd} onAdd={this.addGame} onAddModal={this.showAddModal} />
+                <div className="text-center">
+                    <div id="games" className="row mt-30">
+
+
+                        {games.map((game, index) => (
+                            <div className="col-md-4 col-sm-6" key={index}>
+                                <div className="box16" >
+                                    <ImageHeader imageSrc={game.photo} />
+                                    <div className="box-content">
+                                        <ul className="social">
+                                            <h3 className="title">{game.name}</h3>
+                                            <span className="post">Plataform: {game.platform}</span>
+                                            <br />
+                                            <span className="post">Rate: {game.rate}</span>
+                                            <br />
+                                            <span className="post">ID: {game._id}</span>
+                                            <button type="button" className="btn btn-labeled btn-warning btn-letra" onClick={() => { this.showEditModal(index) }}>
+                                                <span className="btn-label"><i className="glyphicon glyphicon-pencil"></i></span>   Edit   </button>
+                                            <br />
+                                            <button type="button" className="btn btn-labeled btn-danger btn-letra" onClick={() => { this.deleteGame(index)}}>
+                                                <span className="btn-label"><i className="glyphicon glyphicon-trash"></i></span>   Delete   </button>
+
+                                            <EditGame onShow={this.state.showEdit} onEdit={this.editGame}
+                                            onEditModal={() => { this.showEditModal(this.state.currentlyEditing) }}
+                                            currentlyEditing={this.state.currentlyEditing}
+                                            game = {games[this.state.currentlyEditing]}/>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                        ))}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
+ReactDOM.render(<Games />, document.getElementById('app'));
+registerServiceWorker();
